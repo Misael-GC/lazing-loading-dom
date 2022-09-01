1ac60fe1bcc92f89c6537ce263c3963ac6ed5b8c
Misael-GC <Misael-GC@users.noreply.github.com>
2022-09-01 19:09:08 +0000

    deploy: 66b13e2ebea3185d3e4972f27954761f448aedf4

---
+++ _dist_/index.js
@@ -0,0 +1,47 @@
+/**
+ * This file is just a silly example to show everything working in the browser.
+ * When you're ready to start on your site, clear the file. Happy hacking!
+ **/
+
+console.log('Happy hacking :)')
+//crear (1) image
+//agregar #image
+//referencia
+
+//formula del random
+const min = 1;
+const max = 122;
+const random = () => Math.floor(Math.random()* (max - min)) + min;
+
+
+const createImageNode =  () => {
+    const container = document.createElement('div');//creamos el item para el contenedor
+    container.className = 'p-4';
+
+    const image = document.createElement('img'); //creamos la img
+    image.className = 'mx-auto'; //podemos aplicar tailwind
+    image.width = '320'
+    image.src =`https://randomfox.ca/images/${random()}.jpg`; //TO Do
+
+    container.appendChild(image);//le metemos la imagen al contenedor
+    return container;
+};
+
+
+const nuevaImg = createImageNode(); //lo referenciamos a una variable
+const mountNode = document.getElementById('images') //lo guardamos en una referencia porque lo vamos a usar much
+
+//creamos el boton que traiga un nuevo
+const addButton = document.querySelector('button');
+
+//accion del boton agregar img
+const addImage = () => {
+    const newImage = createImageNode(); //se crea un contenedor con la img
+    mountNode.append(newImage) //lo metemos a la etiqueta ancla del HTML
+
+};
+
+addButton.addEventListener('click', addImage)
+
+
+
